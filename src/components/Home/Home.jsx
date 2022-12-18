0816503f027a6b9d2bfce3d7d4f1398538f23f5c
user <ujjalroy7862@gmail.com>
2022-12-18 20:13:59 +0600

    i faceing some problem

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,26 +1,16 @@
 import React, { useEffect } from "react";
 import MovieListing from "../MovieListing/MovieListing";
-import movieApi from "../../common/apis/movieApi";
-import { APIkey } from "../../common/apis/MovieApiKey";
 import { useDispatch } from "react-redux";
-import { addMovies } from "../../features/movies/movieSlice";
+import { fetchAsyncMovies } from "../../features/movies/movieSlice";
 
 const Home = () => {
-  const movieText = "Harry";
+
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const fetchMovie = async () => {
-      const response = await movieApi
-        .get(`?apikey=${APIkey}&s=${movieText}&type=movie`)
-        .catch((err) => {
-          console.log("Error is: ", err);
-        });
-      dispatch(addMovies(response.data));
-    };
-    fetchMovie();
-  }, [dispatch]);
-
+    dispatch(fetchAsyncMovies())
+  }, [dispatch])
+  
   return (
     <div>
       <div className="banner-img">
--- src/features/movies/movieSlice.js
+++ src/features/movies/movieSlice.js
@@ -1,4 +1,19 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import movieApi from "../../common/apis/movieApi";
+import { APIkey } from "../../common/apis/MovieApiKey";
+
+export const fetchAsyncMovies = createAsyncThunk(
+  "movies/fetchAsyncMovies",
+  async () => {
+    const movieText = "Harry";
+    const response = await movieApi
+      .get(`?apikey=${APIkey}&s=${movieText}&type=movie`)
+      .catch((err) => {
+        console.log("Error is: ", err);
+      });
+    return response.data;
+  }
+);
 
 const initialState = {
   movies: {},
@@ -11,6 +26,20 @@ const movieSlice = createSlice({
     addMovies: (state, action) => {
       state.movies = action.payload;
     },
+
+    extraReducers: {
+      [fetchAsyncMovies.pending]: () => {
+        console.log("Pending");
+      },
+      [fetchAsyncMovies.fulfilled]: (state, action) => {
+        console.log("Data is Fetched Successfully");
+        return { ...state, movies: action.payload };
+      },
+      [fetchAsyncMovies.rejected]: () => {
+        console.log("Rejected");
+      },
+    },
+    
   },
 });
 
