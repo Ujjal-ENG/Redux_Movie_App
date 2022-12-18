5d79bbafa159401f7859c0ea23f606b4acb3ff01
user <ujjalroy7862@gmail.com>
2022-12-18 16:00:27 +0600

    complete the routing setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import './App.scss';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import MovieDetail from "./components/MovieDetail/MovieDetail";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
+import Footer from "./components/Footer/Footer";
+
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-      <h1>This is Mama Our App</h1>
+      <Router>
+        <Header></Header>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movie/:imdbID" element={<MovieDetail />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
