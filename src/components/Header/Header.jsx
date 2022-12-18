ab7bf41471849f087b2b1c9c279224650dd44ef2
user <ujjalroy7862@gmail.com>
2022-12-18 16:32:51 +0600

    now adding the scss file

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import img from "../../images/user.png";
+
+import './Header.scss'
 
 const Header = () => {
   return (
-    <div >
-
+    <div className="header">
+      <Link to="/">
+        <div className="logo">Movie App</div>
+      </Link>
+      <div className="user-image">
+        <img src={img} alt="user" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
