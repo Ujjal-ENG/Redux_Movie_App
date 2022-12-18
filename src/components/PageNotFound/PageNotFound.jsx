9301b090d0fe4b41aec72d0ddce75eb114708b63
user <ujjalroy7862@gmail.com>
2022-12-18 19:57:10 +0600

    done the error page

--- src/components/PageNotFound/PageNotFound.jsx
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+/* eslint-disable jsx-a11y/iframe-has-title */
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "./PageNotFound.scss";
 
 const PageNotFound = () => {
   return (
-    <div>PageNotFound</div>
-  )
-}
+    <div className="container">
+      <h1>Sorry This Page is Not Found</h1>
 
-export default PageNotFound
+      <iframe src="https://embed.lottiefiles.com/animation/98642" />
+
+      <div className="button-container">
+        <Link to="/">
+          <button>Go back To Home Page</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default PageNotFound;
